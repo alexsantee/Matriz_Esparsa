0606ebd0ca45bae763bed0dfef6e99f52f7fe8af
felipearb <39956315+felipearb@users.noreply.github.com>
2018-06-14 16:09:38 -0300

    soma_linha e soma_coluna

--- matriz.h
+++ matriz.h
@@ -8,5 +8,7 @@ void apaga_matriz(T_MAT *mat);
 double le_elem(int i, int j, T_MAT *mat);
 void adiciona_elem(int i, int j, double valor, T_MAT *mat);//Apenas para teste
 void remove_elem(int i, int j, T_MAT *mat);
+double soma_linha(int i,T_MAT*mat);
+double soma_coluna(int j,T_MAT*mat);
 
 #endif
