a55f22aad51a549d859c186c3c57220f4473c1fc
felipearb <39956315+felipearb@users.noreply.github.com>
2018-06-15 20:48:44 -0300

    interface montada e atualizada as dependências

--- main.c
+++ main.c
@@ -1,71 +1,148 @@
 #include <stdio.h>	//usado na interação com o usuário
-#include <stdlib.h>	//usado para fazer um vetor de matrizes
+#include <locale.h> //usado para colocar a escrida em português
+
 #include "matriz.h" //usado para administrar as matrizes
 
-//void imprime_matriz(T_MAT mat);
 int menu();
 void clear();
-
+void pause();
 
 int main(int argc, char *argv[]){
-	int opcoes;
-	char alt;
-	do{
+	char resp_user;
+	int m,n;
+	double valor;
+	int existe_matriz=0;
+    T_MAT *mat;
+	
+	do  {
 		clear();
-		opcoes = menu();
-
-		if(opcoes == 1){
-
-		}
-		else if (opcoes == 2){
-		}
-		else if (opcoes == 3){
-		}
-		else if(opcoes == 4){
-			printf("Certeza que deseja sair?[S/N]\n>> ");
-			scanf(" %c", &alt);
-			while(alt != 'S' && alt != 's' && alt != 'n' && alt != 'N'){
-				printf("Opcao inválida, digite novamente.>> \n");
-				scanf(" %c", &alt);
-			}
-		}
-	}while(opcoes != 's' && opcoes != 'S');
+		resp_user = menu();
+		if(resp_user == '1')
+            {
+            printf("***** Setor de crição de matriz *****\n");
+            printf("Insira o valor m(numero de linhas da matriz:");
+            scanf(" %d",&m);
+            printf("Insira o valor n(numero de calonas da matriz:");
+            scanf(" %d",&n);
+            mat = cria_matriz(m,n);	//Cria matriz de tamanho mn e dá ponteiro para ela
+            puts("Matriz criada.\n");
+            existe_matriz=1;
+            pause();
+            }
+		if(resp_user == '2')
+            {
+            printf("***** Setor de exclusão de matriz *****\n");
+            apaga_matriz(T_MAT *mat);
+            puts("Matriz excluida.")
+            existe_matriz=0;
+            pause();
+            }
+		if(resp_user == '3')
+            {
+                if(existe_matriz==1)
+                {
+                printf("***** Setor de valor de uma posição *****\n");
+                printf("Insira o valor da posição j:\n");
+                scanf(" %d",&i);
+                printf("Insira o valor da posição i:\n");
+                scanf(" %d",&j);
+                valor = le_elem(i,j, T_MAT *mat);
+                printf("Na posição %d,%d o valor do elmento é %0.2ld.\n",valor);
+                pause();
+                }else{puts("Você não defeniu matriz.");}
+            }
+        if(resp_user == '4')
+            {
+                if(existe_matriz==1)
+                {
+                printf("***** Setor de soma dos valores de uma linha *****\n");
+                printf("Qual linha:\n");
+                scanf(" %d",&i);
+                valor = soma_linha( i,T_MAT*mat);
+                printf("O valor da  soma dos valores da linha %d é %0.2ld.\n",i,valor);
+                pause();
+                }else{puts("Você não defeniu matriz.");}
+            }
+		if(resp_user == '5')
+            {
+                if(existe_matriz==1)
+                {
+                printf("***** Setor de soma dos valores de uma coluna *****\n");
+                printf("Qual coluna:\n");
+                scanf(" %d",&i);
+                valor = soma_coluna(j,T_MAT*mat);
+                printf("O valor da  soma dos valores da coluna %d é %0.2ld.\n",j,valor);
+                pause();
+                }else{puts("Você não defeniu matriz.");}
+            }
+        if(resp_user == '6')
+            {
+                if(existe_matriz==1)
+                {
+                printf("***** Atribuição de  valor em uma posição *****\n");
+                printf("Insira o valor da posição i:\n");
+                scanf(" %d",&i);
+                printf("Insira o valor da posição j:\n");
+                scanf(" %d",&j);
+                printf("Qual valor:\n");
+                scanf(" ld",&valor);
+                adiciona_elem(i,j, double valor, T_MAT *mat);
+                printf("Na posição %d,%d o valor colocado foi %ld.\n",i,j,valor);
+                pause();
+                }else{puts("Você não defeniu matriz.");}
+            }
+		if(resp_user == '7')
+            {
+                if(existe_matriz==1)
+                {
+                    i = n_linhas(T_MAT *mat);
+                    j = n_colunas(T_MAT *mat);
+                    if(i==j)
+                    {
+                    printf("***** Setor do determinante *****\n");
+                    valor = determinante(T_MAT *mat);
+                    printf("O valor do determinante é : 0.2ld \n",valor;
+                    pause();
+                    }
+                }else{puts("Você não defeniu matriz.");}
+            }
+        
+        }while(resp_user !='0');
 
 	return 0;
 }
 
-int menu(){
-	char opcao_menu;
-	int opcao_menu_int;
-	printf("Bem-vindo ao programa de matrizes esparsas!\n\n");
-	printf("***** MENU *****\n");
-	printf("1.Consultar um valor na matriz\n");
-	printf("2.Imprimir a matriz\n");
-	printf("3.Alterar um valor na matriz\n");
-	printf("4.Criar uma matriz\n");
-	printf("0.Sair\n>> ");
-	scanf(" %d", &opcao_menu);
-	return opcao_menu;
+char menu()
+{
+    char resp;
+    do  {
+        printf("Bem-vindo ao programa de matrizes esparsas!\n\n");
+        printf("***** MENU *****\n");
+        printf("1.Criação da matriz m por n.\n");
+        printf("2.Exclusão da matriz.\n");
+        printf("3.Consulta dos valores de uma posição (i, j) da matriz.\n");
+        printf("4.Consulta da soma dos valores de cada linha da matriz.\n");
+        printf("5.Consulta da soma dos valores de cada coluna da matriz\n");
+        printf("6. Atribuição de um valor na posição (i, j) da matriz\n");
+        printf("7. Calcular o determinante da matriz (2.0)\n");
+        printf("0.Sair\n");
+        scanf(" %c", &resp);
+        }while(resp<'0'||resp>'7');
+        return resp;
 }
 
-void clear(){
+void clear()
+{
 	#ifdef WIN32
    		system("CLS");
 	#else
     		system("clear");
 	#endif
 }
-/*
-Reescrever sem usar diretamente as variáveis da matriza, mas usando apenas
-comandos de "matriz.h"
-void imprime_matriz(T_MAT mat){
-	int i;
-	int j;
-	for (i=0; i<mat.m; i++){
-		for (j=0; j<mat.n; j++){
-			printf("%.1f ", le_elem(i,j,mat.first));
-		}
-		printf("\b\n");
-	}	
+
+void pause()
+{
+    char pausa;
+    puts("Aperte enter")
+    while( ( pause=getchar() ) != '\n' && pause != '\0');   //Pula caracteres lixo
 }
-*/
