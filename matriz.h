5870f6d4900891c450da0a5e774cd1cb8c0fa37d
alexsantee <alexsantee@hotmail.com>
2018-06-15 12:09:56 -0300

    Funções n_linhas e n_colunas

--- matriz.h
+++ matriz.h
@@ -5,6 +5,8 @@ typedef struct matriz T_MAT;
 
 T_MAT *cria_matriz(int m, int n);	//Cria matriz de tamanho mn e dá ponteiro para ela
 void apaga_matriz(T_MAT *mat);	//Limpa memória reservada para uma matriz
+int n_linhas(T_MAT *mat);	//Retorna o número de linhas de uma matriz
+int n_colunas(T_MAT *mat);	//Retorna o número de colunas de uma matriz
 double le_elem(int i, int j, T_MAT *mat);	//Dá o valor do elemento na posição ij
 void adiciona_elem(int i, int j, double valor, T_MAT *mat);	//Define/substitui valor de ij
 void remove_elem(int i, int j, T_MAT *mat);	//Remove elemento da posição ij
